a893f9da6242a10e645d67c0246c5d2abd4eb24c
Mrcsrobles <mrcsrobles@gmail.com>
2020-05-04 21:50:49 +0200

    Se han añadido mejoras pero el pyhcharm no funciona

--- Nonograma.py
+++ Nonograma.py
@@ -2,47 +2,60 @@ from copy import deepcopy
 from pprint import pprint
 from time import sleep
 
+
 # F.inicio
+
+def CrearTablero(data):
+    T = []  # Para crear el tablero se ponen al principio de cada fila el número de # que va a haber
+    for i in range(0, len(data["rows"])):
+        T.append([])
+        for j in range(0, len(data["cols"])):
+            if j < data["rows"][i]:
+                T[-1].append(1)
+            else:
+                T[-1].append(0)
+    return T
+
+
 def Iniciar(*args):
     r, c = map(int, args[0].strip().split())
-    print(r,c)
+    print(r, c)
     data = {
         "rows": list(map(int, args[1].strip().split())),
         "cols": list(map(int, args[2].strip().split()))
     }
     if len(data["rows"]) != r or len(data["cols"]) != c or any(row > c for row in data["rows"]) or any(
             col > r for col in data["cols"]):
-        #Si uno de los datos es incorrecto en la entrada devuelve imposible
+        # Si uno de los datos es incorrecto en la entrada devuelve imposible
         return "IMPOSIBLE"
-    else:
-        T = []
-        for i in range(0, len(data["rows"])):
-            T.append([])
-            for j in range(0, len(data["cols"])):
-                if j < data["rows"][i]:
-                    T[-1].append(1)
-                else:
-                    T[-1].append(0)
-        aux = __rec__(T, data, 0)
-        if not aux[0]:
+    else:  # Se crea el tablero
+        T = CrearTablero(data)
+        sol = __rec__(T, data, 0)  # Devuelve una tupla booleano, nonograma
+        if not sol[0]:
             return "IMPOSIBLE"
         else:
-            return aux[1]
+            return sol[1]
 
 
 # F. Factibilidad
-def Factible(T, row):
+def Factible(T, row, data):
     for col in range(0, len(T[0])):
         iniciado = False
         acabado = False
+        c = 0
         for rows in range(0, row + 1):
             val = T[rows][col]
             if val == 0 and iniciado:
-                acabado = True
+                acabado = True  # Si después de un uno hay un cero se da por terminado
             elif val == 1 and acabado:
-                return False
-            elif val == 1:
-                iniciado = True
+                return False  # Si hay un uno después de haber acabado no es factible ya que hay un espacio
+            elif val == 1 and not iniciado:
+                iniciado = True  # El primer uno empiza a contar
+                c += 1
+            elif val == 1 and iniciado:
+                c += 1  # El resto de unos suman
+            if c > data["cols"][col]:
+                return False  # Si hay más # de los indicados en la columna no es factible
     return True
 
 
@@ -81,12 +94,14 @@ def Desplazar(T, row, data):
             return True
         else:
             return False
-    except:
-        return False
+    except ValueError: # si no hay 1 siempre va a ser desplazable
+        return True
 
 
 def __rec__(T, data, row):
     t = deepcopy(T)
+    if row == 4:
+        print("")
     if row == len(t) - 1:
         desplazable = True
         sol = False
@@ -102,18 +117,19 @@ def __rec__(T, data, row):
     else:
         avanzable = True
         solucionado = False
+
         while avanzable and not solucionado:
-            if Factible(t, row):
+            if Factible(t, row,data):
                 result = __rec__(t, data, row + 1)
                 if result[0]:  # Se comprueba la siguiente fila
                     return (True, result[1])
                 else:
-                    avanzable = Desplazar(t, row, data)
+                    avanzable = Desplazar(t, row, data)# Si los inferiores no dan solución se avanza la actual
             else:
-                avanzable = Desplazar(t, row, data)
-                for i in range(1, len(T)):
-                    Desplazar(t, row + i, data)
-
+                for i in range(1, len(T)-row):
+                    avanzable = Desplazar(t, row+i, data)
+                    if not avanzable:
+                            break
         return (solucionado, [])
 
 
@@ -132,5 +148,6 @@ def Convertir(T):
 from time import time
 
 tini = time()
-pprint(Iniciar("10 5", "1 2 2 2 2 2 1 1 2 1", "1 8 5 1 1"))
+pprint(Iniciar("5 5", "1 3 5 3 1",
+                                 "1 3 5 3 1"))
 print(time() - tini)
